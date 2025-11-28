70b188307e8825fdd495f6eb0fb9d189504b10e3
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2025-11-28 14:59:07 -0300

    ADD: criando database, tabela e começando insert into

---
+++ src/model/turma_model.py
@@ -0,0 +1,7 @@
+from conexao import criar_conexao
+class Turma_model():
+
+    def inserir_turma(self,nome,padrinho) -> bool:
+        conexao, cursor = criar_conexao()
+
+        cursor.execute("""INSERT INTO""")
